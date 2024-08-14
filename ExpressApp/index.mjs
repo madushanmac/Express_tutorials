7e2534d54e47badfce267ebf8b153e5e0244f03e
madushanmac <Chamindumadushan1996org@gmail.com>
2024-08-14 18:57:02 +0530

    cvbc

--- ExpressApp/index.mjs
+++ ExpressApp/index.mjs
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 
 const app = express();
 const mockUsers = [
@@ -12,14 +12,40 @@ app.get("/test", (req, res) => {
 });
 
 app.get("/api/users/:id", (req, res) => {
-  //res.status(200).json(mockUsers);
   const parseId = parseInt(req.params.id); // Convert id to parseInt
   if (isNaN(parseId)) return res.status(401).send({ msg: "Bad Request" });
 
   const findUsers = mockUsers.find((user) => user.id === parseId);
   if (!mockUsers || !findUsers)
-    return res.status(401).send({ msg: "Users Empty " });
-  return res.status(201).send(findUsers);
+    return res.status(401).send({ msg: "users empty" });
+  return res.status(201).json(findUsers);
+});
+
+app.get("/api/users/", (req, res) => {
+  // const parseId = parseInt(req.params.id); // Convert id to parseInt
+  // if (isNaN(parseId)) return res.status(401).send({ msg: "Bad Request" });
+
+  console.log(req.query);
+
+  const {
+    query: { filter, value },
+  } = req;
+  if (filter && value)
+    return res.json(mockUsers.filter((user) => user[filter].includes(value)));
+  return response.json(mockUsers);
+
+  if (mockUsers == 0) return res.status(401).send({ msg: "users not found" });
+  return res.json(users);
+});
+
+app.get("/api/products/", (req, res) => {
+  const products = [
+    { id: 100, productName: "Solor", price: 2500.0 },
+    { id: 200, productName: "Malcl", price: 5500.0 },
+    { id: 300, productName: "RedRum", price: 9500.0 },
+  ];
+
+  res.json(products);
 });
 
 // Start the server
